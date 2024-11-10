efb139d66d8270112fb214c1c75e45ee13521723
Plcc18 <pedrolucascabral4@gmail.com>
2024-11-10 00:40:34 -0300

    app

--- script.js
+++ script.js
@@ -68,3 +68,15 @@ const showAlert = (message) => {
 iconDelete.addEventListener("click", () => {
   footer.hidden = true
 })
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log("Service Worker registrado com sucesso:", registration);
+      })
+      .catch(error => {
+        console.log("Falha ao registrar o Service Worker:", error);
+      });
+  });
+}
